a6d34ea2a2acb1cbdd184a9254e8d8809bb2b761
Arghya <arghya.official@yahoo.com>
2024-06-17 19:02:26 +0530

    Added two.sh

---
+++ OS/Assignment 1/two.sh
@@ -0,0 +1,8 @@
+echo "Enter base salary: "
+read b
+read -p "Enter the percentage of da: " da
+daAmount=`expr $b \* $da`
+daAmount=$(echo "scale=2; $daAmount / 100 | bc)
+echo "daAmount = $daAmount"
+net=$(echo "scale=2; $b+$daAmount" | bc)
+echo "Basic Salary: $b, da: $da, daAmount: $daAmount, Net salary is: $net"
